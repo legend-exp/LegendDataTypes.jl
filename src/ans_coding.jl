1820fe97b229695427001cde9d93d891d0c26b55
Oliver Schulz <oschulz@mpp.mpg.de>
2023-05-12 15:18:08 +0200

    STASH ANS

--- src/ans_coding.jl
+++ src/ans_coding.jl
@@ -1,20 +1,30 @@
 # This file is a part of LegendDataTypes.jl, licensed under the MIT License (MIT).
 
-function _ans_gen_pdf(::Type{TData}, symbol_pmf::Function) where TData<:Real
+
+_binary_log(n::T) where {T<:Integer} = 8*sizeof(T) - leading_zeros(n+n-one(T)) - 1
+
+
+function _ans_gen_pdf_cdf(::Type{TData}, symbol_pmf::Function) where TData<:Real
     TStream = widen(unsigned(TData)) # TBuf = widen(TStream) is implied
-    m = typemax(TStream) - typemin(TStream) + 1
-    n = _binary_log(typemax(TStream) - typemin(TStream) + 1)
+    m = Int(typemax(TStream)) - Int(typemin(TStream)) + 1
+    n = _binary_log(m)
     @assert m == 1<<n
 
-    full_length_pdf = typemax(TData) - typemin(TData) + 1
-    s_cutoff = min(ceil(Int, 2 * quantile(d_signed, cdf(Normal(), 3))), full_length_pdf - 1)
+    n_symbols = Int(typemax(TData)) - Int(typemin(TData)) + 1
+
+    s_cutoff::Int = 0
+    total_prob::Float64 = 0
+    while total_prob < 0.99865 && s_cutoff < (n_symbols - 1)
+        total_prob += symbol_pmf(s_cutoff)
+        s_cutoff += 1
+    end
 
     float_PDF = [symbol_pmf(s) for s in 0:s_cutoff];
     # Tail entries have implicit PDF of one, add one to all explicit entries as well:
-    float_PDF .= float_PDF .* ((m - full_length_pdf) / sum(float_PDF)) .+ 1
+    float_PDF .= float_PDF .* ((m - n_symbols) / sum(float_PDF)) .+ 1
 
     PDF = round.(Int, float_PDF)
-    n_tail_entries = full_length_pdf - length(eachindex(PDF))
+    n_tail_entries = n_symbols - length(eachindex(PDF))
 
     sum_PDF_err = (sum(PDF) + n_tail_entries) - m
     # ToDo: More efficient implementation:
@@ -30,51 +40,60 @@ function _ans_gen_pdf(::Type{TData}, symbol_pmf::Function) where TData<:Real
     # In AND, CDF sums up PDF *until* current entry:
     CDF = circshift(cumsum(PDF), 1)
     CDF[begin] = 0
+
+    (PDF = PDF, CDF = CDF, n_symbols = n_symbols)
 end
 
-function get_ans_pdf(PDF::AbstractVector{<:Integer}, s::Integer, m::Integer)
-    @assert 0 <= s < m
+function _get_ans_pdf(PDF::AbstractVector{<:Integer}, s::Integer, n_symbols::Integer)
+    @assert 0 <= s < n_symbols
     T = eltype(PDF)
-    s < m ? PDF(firstindex(PDF) + s) : one(T)
+    @assert s < n_symbols
+    s < length(eachindex(PDF)) ? PDF[firstindex(PDF) + s] : one(T)
 end
 
 
-function get_ans_cdf(CDF::AbstractVector{<:Integer}, s::Integer, m::Integer)
-    @assert 0 <= s < m
+function _get_ans_cdf(CDF::AbstractVector{<:Integer}, s::Integer, n_symbols::Integer)
+    @assert 0 <= s < n_symbols
     T = eltype(CDF)
     l = length(eachindex(CDF))
-    s < m ? CDF(firstindex(CDF) + s) : T(last(CDF) + s - l + 1)
+    @assert s < n_symbols
+    s < length(eachindex(CDF)) ? CDF[firstindex(CDF) + s] : T(last(CDF) + s - l + 1)
 end
 
 
-function _ans_encode!(output::AbstractVector{TStream}, data::AbstractVector{<:Integer}) where {TStream<:Unsigned}
-    @assert axes(PDF) == axes(CDF)
-    i0 = firstindex(PDF)
-
+function _ans_encode!(
+    output::AbstractVector{TStream}, data::AbstractVector{TData},
+    PDF::AbstractVector{Int}, CDF::AbstractVector{Int}
+) where {TStream <: Unsigned, TData <: Integer}
     TBuf = widen(TStream)
-    sum_PDF = sum(PDF)
-    n = _binary_log(sum_PDF)
-    @assert sum_PDF == 1<<n
+
+    m = Int(typemax(TStream)) - Int(typemin(TStream)) + 1
+    n = _binary_log(m)
+    n_symbols = Int(typemax(TData)) - Int(typemin(TData)) + 1
 
     d = (8 * sizeof(TBuf)) - n
     encstream_nbits = 8 * sizeof(TStream)
 
-    x::TBuf = zero(TBuf)
+    @info "DEBUG A" TBuf m n n_symbols d encstream_nbits
 
+    x::TBuf = zero(TBuf)
     for s in data
-        if x >= (PDF[s + i0] << d)
+        pdf_s = TBuf(_get_ans_pdf(PDF, s, n_symbols))
+        cdf_s = TBuf(_get_ans_cdf(CDF, s, n_symbols))
+        @info "DEBUG B" x s pdf_s cdf_s
+
+        if x >= (pdf_s << d)
+            @info "DEBUG C push" unsafe_trunc(TStream, x)
             push!(output, unsafe_trunc(TStream, x))
             x = x >> encstream_nbits
         end
-
-        pdf_s = TBuf(PDF[s + i0])
-        cdf_s = TBuf(CDF[s + i0])
 
         # Accelerate by caching SignedMultiplicativeInverse{TBuf} for PDF:
         x = (div(x, pdf_s) << n) + mod(x, pdf_s) + cdf_s
     end
 
     while(x > 0)
+        @info "DEBUG D push" x unsafe_trunc(TStream, x)
         push!(output, unsafe_trunc(TStream, x))
         x = x >> (8 * sizeof(TStream))
     end
